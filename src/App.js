a4a5e65e73ac5da59c6610be362948ae4dbcf985
David Kiama <davidkiama98@gmail.com>
2025-11-06 18:48:02 +0300

    Change hover color for the checkboxes

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import React, { useState } from "react";
 import "./App.scss";
 
 function App() {
-  const [selectedPages, setSelectedPages] = useState(new Set([2, 3, 4])); // Pages 2–4 initially selected or not
+  const [selectedPages, setSelectedPages] = useState(new Set());
 
   const togglePage = (pageNum) => {
     setSelectedPages((prev) => {
       const newSet = new Set(prev);
-      if (newSet.has(pageNum)) {
-        newSet.delete(pageNum);
-      } else {
-        newSet.add(pageNum);
-      }
+      if (newSet.has(pageNum)) newSet.delete(pageNum);
+      else newSet.add(pageNum);
       return newSet;
     });
   };
 
   const toggleAllPages = () => {
-    if (selectedPages.size === 3) {
-      setSelectedPages(new Set());
-    } else {
-      setSelectedPages(new Set([2, 3, 4]));
-    }
+    if (selectedPages.size === 3) setSelectedPages(new Set());
+    else setSelectedPages(new Set([2, 3, 4]));
   };
 
   const handleDone = () => {
@@ -31,46 +25,51 @@ function App() {
   };
 
   const getCheckboxSrc = (pageNum) => {
-    const isChecked = pageNum === 1 || selectedPages.has(pageNum);
-    const isDisabled = pageNum === 1;
-
-    if (!isChecked) return "/svg/Property 1=Default.svg";
-    if (isDisabled) return "/svg/Property 1=Variant2.svg";
-    return "/svg/Property 1=Variant5.svg";
+    if (pageNum === 1) return "/svg/checkbox-disabled.svg";
+    return selectedPages.has(pageNum) ? "/svg/checkbox-enabled.svg" : "/svg/checkbox-default.svg";
   };
 
   const isAllSelected = selectedPages.size === 3;
 
   return (
-    <div className="frame">
-      <div className="pages-content">
-        {/* All Pages */}
-        <div className={`checkbox-item ${isAllSelected ? "checked" : ""}`} onClick={toggleAllPages}>
-          <span className="checkbox-label">All pages</span>
-          <img src={getCheckboxSrc(0)} alt="checkbox" className="checkbox-icon" />
-        </div>
-
-        {/* Page 1 - Hardcoded: always selected + disabled */}
-        <div className="checkbox-item checked ">
-          <span className="checkbox-label">Page 1</span>
-          <img src="/svg/Property 1=Variant2.svg" alt="checkbox" className="checkbox-icon" />
-        </div>
-
-        {/* Pages 2, 3, 4 - Dynamic */}
-        {[2, 3, 4].map((pageNum) => (
+    <div id="container">
+      <div className="frame">
+        <div className="pages-content">
+          {/* All Pages */}
           <div
-            key={pageNum}
-            className={`checkbox-item ${selectedPages.has(pageNum) ? "checked" : ""}`}
-            onClick={() => togglePage(pageNum)}
+            className={`checkbox-item ${isAllSelected ? "checked" : ""}`}
+            onClick={toggleAllPages}
           >
-            <span className="checkbox-label">Page {pageNum}</span>
-            <img src={getCheckboxSrc(pageNum)} alt="checkbox" className="checkbox-icon" />
+            <span className="checkbox-label">All pages</span>
+            <img
+              src={isAllSelected ? "/svg/checkbox-enabled.svg" : "/svg/checkbox-default.svg"}
+              alt="checkbox"
+              className="checkbox-icon"
+            />
           </div>
-        ))}
 
-        <button className="done-button" onClick={handleDone}>
-          Done
-        </button>
+          {/* Page 1 - Disabled */}
+          <div className="checkbox-item ">
+            <span className="checkbox-label">Page 1</span>
+            <img src={getCheckboxSrc(1)} alt="checkbox" className="checkbox-icon" />
+          </div>
+
+          {/* Pages 2–4 */}
+          {[2, 3, 4].map((pageNum) => (
+            <div
+              key={pageNum}
+              className={`checkbox-item ${selectedPages.has(pageNum) ? "checked" : ""}`}
+              onClick={() => togglePage(pageNum)}
+            >
+              <span className="checkbox-label">Page {pageNum}</span>
+              <img src={getCheckboxSrc(pageNum)} alt="checkbox" className="checkbox-icon" />
+            </div>
+          ))}
+
+          <button className="done-button" onClick={handleDone}>
+            Done
+          </button>
+        </div>
       </div>
     </div>
   );
